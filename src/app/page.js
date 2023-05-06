0f3fe30efde2f0447a51fa542dccc9be78df7739
zhangxin <wb_zhangxin29@meituan.com>
2023-05-06 15:02:43 +0800

    feat: 初始化项目

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Button from "@/components/button/button"
+
+export default function Home() {
+  return (
+    <main>
+      <Button>ceshi</Button>
+    </main>
+  )
+}
---
+++ src/components/button/button.jsx
@@ -0,0 +1,7 @@
+function Button({ onClick, children }) {
+  return (
+    <button onClick={onClick}>{children}</button>
+  );
+}
+
+export default Button;
