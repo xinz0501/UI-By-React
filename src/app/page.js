ffdb36ebc153e0607c15bea8bfc6665e27804759
zhangxin <wb_zhangxin29@meituan.com>
2023-05-08 20:17:37 +0800

    feat: 重新使用vite构建项目

--- src/app/page.js
+++
@@ -1,9 +0,0 @@
-import Button from "@/components/button/button"
-
-export default function Home() {
-  return (
-    <main>
-      <Button>ceshi</Button>
-    </main>
-  )
-}
--- src/components/button/button.jsx
+++
@@ -1,7 +0,0 @@
-function Button({ onClick, children }) {
-  return (
-    <button onClick={onClick}>{children}</button>
-  );
-}
-
-export default Button;
